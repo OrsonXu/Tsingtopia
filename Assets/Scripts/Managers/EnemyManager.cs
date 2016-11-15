14ce09030ad7b8049b8395f1d871a54d7fed58ed
OrsonXu <orson.xu@foxmail.com>
2016-11-15 23:42:44 +0800

    Finish the battle part of the game in unity, doing reconstruction of the code.

---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject[] enemies;
+    public float spawnTime = 3f;
+    public float spawnInterval = 2f;
+    public Transform[] spawnPoints;
+
+    void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnInterval);
+    }
+
+
+    void Spawn ()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int enemyIndex = Random.Range(0, enemies.Length);
+        Instantiate(enemies[enemyIndex], spawnPoints[enemyIndex].position, spawnPoints[enemyIndex].rotation);
+        
+    }
+}
