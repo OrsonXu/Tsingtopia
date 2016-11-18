5c9d416d3bf066363cb3ad1fe2513b7f29af1114
OrsonXu <orson.xu@foxmail.com>
2016-11-18 21:51:13 +0800

    add two test scenes

---
+++ Assets/Scripts/testMovement.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class testMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public float Speed { get; set; }
+
+    private Rigidbody playerRigidbody;
+    private Vector3 movement;
+    private float h;
+    private float v;
+    private Ray camRay;
+    private RaycastHit floorHit;
+    private float camRayLength = 100f;
+    private int floorMask;
+    private Vector3 playerToMouse;
+    private Quaternion newRotation;
+
+    Vector3 moveDirection;
+    bool rightMouseActive;
+    RaycastHit rightMouseRay = new RaycastHit();
+    Vector3 rightMouseTarget;
+    Animator anim;
+
+    void Awake()
+    {
+        Speed = 10;
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+
+        rightMouseTarget = transform.position;
+
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        anim.SetBool("IsWalking", false);
+
+        rightMouseActive = (bool)Input.GetButton("Fire2");
+        // Move the player around the scene.
+        if (h != 0 || v != 0)
+        {
+            anim.SetBool("IsWalking", false);
+            Move(h, v);
+            rightMouseActive = false;
+        }
+
+        //MoveToward();
+
+        // Turn the player to face the mouse cursor.
+        Turning();
+    }
+
+    void Move(float h, float v)
+    {
+        anim.SetBool("IsWalking", true);
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * Speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+
+
+    void MoveToward()
+    {
+        if (rightMouseActive)
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                rightMouseTarget = playerToMouse;
+            }
+
+        }
+        moveDirection = rightMouseTarget - transform.position;
+        moveDirection.y = 0f;
+        if (moveDirection.magnitude > 10)
+        {
+            moveDirection = moveDirection.normalized;
+            playerRigidbody.MovePosition(transform.position + moveDirection.normalized * Speed * Time.deltaTime);
+        }
+
+    }
+
+    void Turning()
+    {
+        camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            playerToMouse = floorHit.point - transform.position;
+
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+            newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+}
