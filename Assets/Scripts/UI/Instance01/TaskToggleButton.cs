91376eee81c1e480356c113dd7cfb2038620688a
Hongpeng Ma <1085170737@qq.com>
2016-11-29 20:49:55 +0800

    Add Task Panel

---
+++ Assets/Scripts/UI/Instance01/TaskToggleButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskToggleButton : MonoBehaviour
+{
+
+    public void TogglePanel (GameObject panel) {
+        panel.SetActive (!panel.activeSelf);
+    }
+}
+
