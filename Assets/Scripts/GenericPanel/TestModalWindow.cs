7656d4ee3570e4be82f1249e4f3759551e941623
Hongpeng Ma <1085170737@qq.com>
2016-12-01 09:37:59 +0800

    Generic Panel Ver 0.2

--- Assets/Scripts/GenericPanel/TestModalWindow.cs
+++ Assets/Scripts/GenericPanel/TestModalWindow.cs
@@ -9,28 +9,20 @@ public class TestModalWindow : MonoBehaviour
     private ModalPanel modalPanel;
     private DisplayManager displayManager;
 
-    private UnityAction myYesAction;
-    private UnityAction myNoAction;
-    private UnityAction myCancelAction;
-
     void Awake()
     {
         modalPanel = ModalPanel.Instance();
         displayManager = DisplayManager.Instance();
-
-        myYesAction = new UnityAction(TestYesFunction);
-        myNoAction = new UnityAction(TestNoFunction);
-        myCancelAction = new UnityAction(TestCancelFunction);
     }
 
     //  Send to the Modal Panel to set up the Buttons and Functions to call
     public void TestYNC()
     {
         modalPanel.Choice("Do you want to spawn this sphere?", TestYesFunction, TestNoFunction, TestCancelFunction);
-        //      modalPanel.Choice ("Would you like a poke in the eye?\nHow about with a sharp stick?", myYesAction, myNoAction, myCancelAction);
+        
     }
 
-    //  These are wrapped into UnityActions
+    //  Test functions
     void TestYesFunction()
     {
         displayManager.DisplayMessage("Heck yeah! Yup!");
