798942155ffa6f5fd78bf67fde4b9526f93e7c29
hp Ma <hp Ma>
2016-12-22 21:36:44 +0800

    Fixed a bug in task sys

--- Assets/Scripts/ControlSystem/TaskManager/TaskFactory.cs
+++ Assets/Scripts/ControlSystem/TaskManager/TaskFactory.cs
@@ -32,19 +32,21 @@ public class TaskFactory : MonoBehaviour {
 
     }
 	void Start(){
-		InitTasks();
+		InitParam ();
 		ConstructTaskGraph();
+		InitTasks();
+
 	}
-    private void InitTasks()
-    {
-        string name;
-        string description;
-        // Zero
-        name = "";
-        description = "";
-        _taskID.Add(0);
-        _taskName.Add(name);
-        _taskDescription.Add(description);
+
+	private void InitParam(){
+		string name;
+		string description;
+		// Zero
+		name = "";
+		description = "";
+		_taskID.Add(0);
+		_taskName.Add(name);
+		_taskDescription.Add(description);
 
 		for (int i = 0; i < names.Length; i++) {
 			Debug.Log ("************");
@@ -52,46 +54,13 @@ public class TaskFactory : MonoBehaviour {
 			_taskName.Add(names[i]);
 			_taskDescription.Add(descriptions[i]);
 		}
+	}
+    private void InitTasks()
+    {
+        
 		TaskManager.activeListAdd (1);
-		TaskManager.activeListAdd (2);
-        //TaskManager.activeListAdd (3);
-//		// First one
-//
-//        name = "开始游戏";
-//        description = "";
-//        _taskID.Add(++_taskNumberCounter);
-//        _taskName.Add(name);
-//        _taskDescription.Add(description);
-//        // Second
-//        name = "拜访董渊老师";
-//        description = "校园里发生了异变，去问问董老师，老师应该知道些什么…";
-//        _taskID.Add(++_taskNumberCounter);
-//        _taskName.Add(name);
-//        _taskDescription.Add(description);
-//        // Third
-//        name = "探索二校门";
-//        description = "二校门处也许有线索…";
-//        _taskID.Add(++_taskNumberCounter);
-//        _taskName.Add(name);
-//        _taskDescription.Add(description);
-//        // Forth
-//        name = "第一个结论";
-//        description = "赶紧把这一发现告诉董老师。";
-//        _taskID.Add(++_taskNumberCounter);
-//        _taskName.Add(name);
-//        _taskDescription.Add(description);
-//        // Fifth
-//        name = "三教的第一场战斗";
-//        description = "进入三教，消灭敌人，那里应该是异变刚开始的地方。";
-//        _taskID.Add(++_taskNumberCounter);
-//        _taskName.Add(name);
-//        _taskDescription.Add(description);
-//        // Sixth
-//        name = "进一步探索";
-//        description = "返回董老师处报告情况。";
-//        _taskID.Add(++_taskNumberCounter);
-//        _taskName.Add(name);
-//        _taskDescription.Add(description);
+		TaskManager.TriggerTask ("Task1Trigger");
+
 
     }
 
