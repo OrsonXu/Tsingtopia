a4213b8cf1d9e66c6414b82194304d4d180a34fb
OrsonXu <orson.xu@foxmail.com>
2016-12-22 20:21:29 +0800

    Add task trigger scripts

---
+++ Assets/Scripts/Task/ErxiaomengTask.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ErxiaomengTask : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            TaskManager.TriggerTask("Task3" + "Trigger");
+        }
+    }
+}
---
+++ Assets/Scripts/Task/NPCTask.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCTask : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            TaskManager.TriggerTask("Task2" + "Trigger");
+            TaskManager.TriggerTask("Task4" + "Trigger");
+        } 
+    }
+}
