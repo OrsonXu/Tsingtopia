b1f70c8a79079619b550b087f99ee3b4ddb6b7f8
OrsonXu <orson.xu@foxmail.com>
2016-12-30 20:48:11 +0800

    Finish the comment on the recover scripts

--- Assets/Scripts/Recover/RecoverHP.cs
+++ Assets/Scripts/Recover/RecoverHP.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class RecoverHP : Recover {
 
-
+    /// <summary>
+    /// Recover the health value when entering the trigger
+    /// </summary>
+    /// <param name="other"></param>
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && !other.isTrigger)
         {
-            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-            playerHealth.ChangeHealth(RecoverValue);
+            //PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            //playerHealth.ChangeHealth(RecoverValue);
+            MessageManager.TriggerEvent("PlayerChangeHealth", RecoverValue);
             DestorySelf();
         }
     }
--- Assets/Scripts/Recover/RecoverMP.cs
+++ Assets/Scripts/Recover/RecoverMP.cs
@@ -2,13 +2,17 @@
 using System.Collections;
 
 public class RecoverMP : Recover {
-
+    /// <summary>
+    /// Recover the health value when entering the trigger
+    /// </summary>
+    /// <param name="other"></param>
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && !other.isTrigger)
         {
-            PlayerMagic playerMagic = other.GetComponent<PlayerMagic>();
-            playerMagic.ChangeMagic(RecoverValue);
+            //PlayerMagic playerMagic = other.GetComponent<PlayerMagic>();
+            //playerMagic.ChangeMagic(RecoverValue);
+            MessageManager.TriggerEvent("PlayerChangeMagic", RecoverValue);
             DestorySelf();
         }
     }
