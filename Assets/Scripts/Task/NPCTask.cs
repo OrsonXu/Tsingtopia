1b972d47c77bb6673e1de644032a9a29c41c89e2
OrsonXu <orson.xu@foxmail.com>
2016-12-30 20:52:01 +0800

    Finish the comment of the task trigger

--- Assets/Scripts/Task/NPCTask.cs
+++ Assets/Scripts/Task/NPCTask.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 
 public class NPCTask : MonoBehaviour {
-
+    /// <summary>
+    /// Trigger the task when enter the trigger of the NPC
+    /// </summary>
+    /// <param name="other"></param>
     public void OnTriggerEnter(Collider other)
     {
         Task task3 = GameObject.Find("TaskSystem/TaskFactory/task3").GetComponent<Task>();
         if (other.tag == "Player")
         {
+            // If the task3 is discovered adn haven't finished, show a simple dialogue
             if (task3.getStatus() == TaskStatus.DISCOVERED)
             {
                 GenericDialogueManager.Instance().DisplayMessage("董老师:\n小清你回来那么快?二校门那里有线索吗?");
